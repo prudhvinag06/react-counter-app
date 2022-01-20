0733accb911b1ce4ade8399b65e2d7a50a127a0f
prudhvinag06 <prudhvinag637@gmail.com>
2022-01-20 15:40:36 +0530

    basic initial counter

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -1,20 +1,35 @@
 import React, { Component } from 'react';
 import './Counter.css';
 
-function Counter(){ //function component
+class Counter extends Component{ //function component
+
+  //Define the initial state in constructor
+  constructor(){
+    super();
+    this.state = {
+      counter : 0
+    }
+    //super();
+    this.increment = this.increment.bind(this); //without this step we can't use state variables in this function "increment"
+
+  }
+  render(){
     return (
       <div className="counter">
-        
-        <button onClick = {increment}>+1</button> 
-        <span className = "count">0</span>
-
-        
+        <button onClick = {this.increment}>+1</button> 
+        <span className = "count">{this.state.counter}</span>
       </div>
     );
   }
-
-  function increment(){
-      console.log('increment');
+    increment(){
+      this.setState({
+        counter : this.state.counter + 1
+      });
+      //console.log('increment');
+    }
   }
+   
+
+  
 
   export default Counter;
